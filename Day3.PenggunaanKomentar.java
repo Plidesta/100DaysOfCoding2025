dcfd999c287c8a001792f268d012af6048855e4d
Plidesta <lidescraft@gmail.com>
2025-09-10 17:06:13 +0800

    Create Day3.PenggunaanKomentar.java

---
+++ Day3.PenggunaanKomentar.java
@@ -0,0 +1,21 @@
+public class main {
+  public static void main(String[] args) {
+        
+        //komentar adalah keterangan dari suatu program
+        //yg tidak akan dibaca sebagai pogram
+        
+        //beberapa format komentar :
+        
+        //komentar untuk 1 baris kalimat
+        
+        /*komentar yg terdiri dari
+         *beberapa baris kalimat
+         */
+        
+        
+        
+        
+        
+    }
+    
+}
