c4847c69cdd59de3fcf2b277260af0d8e4a2466d
Plidesta <lidescraft@gmail.com>
2025-09-13 00:48:15 +0800

    Create Day6.TipeDataAngkaDesimal.java

---
+++ Day6.TipeDataAngkaDesimal.java
@@ -0,0 +1,9 @@
+public class main {
+  public static void main(String[] args) {
+        float TinggiBadan = 80.5f;
+        double BeratBadan = 60.88;
+    System.out.println("tinggi badan = "+TinggiBadan);
+     System.out.println("berat badan = "+BeratBadan);
+    }
+    
+}
