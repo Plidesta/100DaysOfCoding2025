8800d867a4e5e69a3d52ab0d75776107c5070e81
Plidesta <lidescraft@gmail.com>
2025-09-12 08:21:28 +0800

    Create Day5.TipeDataAngkaBulat.java

---
+++ Day5.TipeDataAngkaBulat.java
@@ -0,0 +1,14 @@
+public class main {
+public static void main(String[]args) {
+        
+       byte angka1 = 120;
+       short angka2 = 20000;
+        int angka3 = 1200000;
+        long angka4 = 1400000000;
+        System.out.println("nilai angka 1 = "+angka1);
+        System.out.println("nilai angka 2 = "+angka2);
+        System.out.println("nilai angka 3 = "+angka3);
+        System.out.println("nilai angka 4 = "+angka4);
+    }
+    
+}
