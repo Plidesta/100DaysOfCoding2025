ac9d13772dc71071c7fdfb9047cb46c86712712d
Plidesta <lidescraft@gmail.com>
2025-09-15 00:10:25 +0800

    Create Day8.pengenalanTipeDataString.java

---
+++ Day8.pengenalanTipeDataString.java
@@ -0,0 +1,22 @@
+public class main {
+  public static void main(String[] args) {
+     //pengenalan tipe data string
+     //string digunakan pada tipe data teks
+String teks1 = "nama : plidesta";
+  String teks2 = "nim : D0225312";
+      String teks3 = "saya kuliah di universitas sulawesi barat";  
+        String teks4 = "tepatnya di fakultas teknik";
+        String teks5 = "prodi informatika angkatan 25";
+        String teks6 = "dari kelas A informatika 025";
+        
+        System.out.println(teks1);
+        System.out.println(teks2);
+        System.out.println(teks3);
+        System.out.println(teks4);
+        System.out.println(teks5);
+        System.out.println(teks6);
+        
+        
+    }
+    
+          }
