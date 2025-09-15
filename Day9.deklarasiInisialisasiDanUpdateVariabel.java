0cc5f92bd97188cf0cf83dbc6e998bbccc3a2d61
Plidesta <lidescraft@gmail.com>
2025-09-16 00:01:52 +0800

    Create Day9.deklarasiInisialisasiDanUpdateVariabel.java

---
+++ Day9.deklarasiInisialisasiDanUpdateVariabel.java
@@ -0,0 +1,53 @@
+public class main {
+  public static void main(String[] args) {
+      //deklarasi,inisialisasi dan update variabel
+        
+//deklarasi variabel atau pemberian nama variabel
+        String nama;
+String nim;
+int umur;
+        double beratBadan;
+        float tinggiBadan;
+        char kelas;
+        boolean anakTeknik;
+        boolean dariFakultasLainlain;
+        //inisialisasi atau pemberian nilai ke variabel yg telah dideklarasi tadi
+        nama ="PLIDESTA";
+        nim = "D0225312";
+        umur = 20;
+        beratBadan = 65.5;
+        tinggiBadan = 168.9f;
+        kelas ='A';
+        anakTeknik = true;
+        dariFakultasLainlain = false;
+        System.out.println("SEBELUM UPDATE VARIABEL");
+        System.out.println("NAMA\t:"+nama );
+        System.out.println("NIM\t\t:"+nim);
+        System.out.println("UMUR\t\t:"+umur);
+        System.out.println("BERAT\tBADAN:"+beratBadan);
+        System.out.println("TINGGI\tBADAN:"+tinggiBadan);
+        System.out.println("KELAS\t:"+kelas);
+        System.out.println("anak\tTeknik:"+anakTeknik);
+        System.out.println("fakultas\tlain-lain:"+dariFakultasLainlain);
+        //update variabel
+        nama ="DESTA";
+        nim = "D0225411";
+        umur = 17;
+        beratBadan = 80.5;
+        tinggiBadan = 169.9f;
+        kelas ='B';
+        anakTeknik = false;
+        dariFakultasLainlain = true;
+        System.out.println("SETELAH UPDATE VARIABEL");
+        System.out.println("NAMA\t:"+nama );
+        System.out.println("NIM\t\t:"+nim);
+        System.out.println("UMUR\t\t:"+umur);
+        System.out.println("BERAT\tBADAN:"+beratBadan);
+        System.out.println("TINGGI\tBADAN:"+tinggiBadan);
+        System.out.println("KELAS\t:"+kelas);
+        System.out.println("anak\tTeknik:"+anakTeknik);
+        System.out.println("fakultas\tlain-lain:"+dariFakultasLainlain);
+    }
+    
+          }
+  
