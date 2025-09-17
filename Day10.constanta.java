c5da2edd3a0bc86215468a362e28e30ab2bf2f6c
Plidesta <lidescraft@gmail.com>
2025-09-17 19:15:44 +0800

    Create Day10.constanta.java

---
+++ Day10.constanta.java
@@ -0,0 +1,11 @@
+public class main {
+  public static void main(String[] args) {
+   final String NAMA = "PLIDESTA";
+       final String NIM = "D0225312";
+       final String PROGRAM_STUDY = "INFORMATIKA";
+       
+System.out.println("nama saya: "+NAMA);
+System.out.println("dengan NIM: "+NIM);
+   System.out.println("program study: "+PROGRAM_STUDY);
+    }
+}
