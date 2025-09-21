39cb79d72268d04e0cf4382a27787609ca449179
Plidesta <lidescraft@gmail.com>
2025-09-21 18:46:09 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,17 @@
+public class main {
+  public static void main(String[]args) {
+    String nilai = "986";
+    int angka = Integer.parseInt(nilai);
+        System.out.println(angka);
+        System.out.println("");
+        
+        String nilai2 = "80.55";
+        double angka2 = Double.parseDouble(nilai2);
+        System.out.println(angka2);
+        System.out.println("");
+        
+   String kondisi = "true";
+    boolean kondisi2 = Boolean.parseBoolean(kondisi);
+        System.out.println(kondisi2);
+    }
+}
