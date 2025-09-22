d1009ac00c9aa874ac4c9718ca911fcbc97fa08c
Plidesta <lidescraft@gmail.com>
2025-09-22 21:11:35 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,19 @@
+public class main {
+  public static void main(String[]args) {
+        System.out.println("bilangan bulat");
+      int bil = 20;
+       int bil2 = 40;
+       int bil3 = bil + bil2;
+       int bil4 = bil - bil2;
+        System.out.println("hasil dari 20 + 40 = "+bil3);
+        System.out.println("hasil dari 20 - 40 = "+bil4);
+      System.out.println("bilangan desimal");
+        double bila = 22.2;
+        double bila2= 44.3;
+        double bila3 = bila + bila2;
+        double bila4 = bila - bila2;
+        System.out.println("hasil dari 22.2 + 44.3 = "+bila3);
+        System.out.println("hasil dari 22.2 - 44.3 = "+bila4);
+    }
+    
+}
