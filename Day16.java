69a05395187590e50f4aa1755690aa2cef9168fe
Plidesta <lidescraft@gmail.com>
2025-09-23 22:05:22 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,19 @@
+public class main {
+  public static void main(String[]args) {
+         System.out.println("menggunakan bilangan bulat ");
+        byte nilai = 20;
+         byte nilai2 = 60;
+        int nilai3 = nilai*nilai2;
+         int nilai4 = nilai2/nilai;
+        System.out.println("jadi hasil dari 20 × 60 = "+nilai3);
+         System.out.println("jadi hasil dari 60 / 20 = "+nilai4 );
+        System.out.println("menggunakan bilangan desimal ");
+        double angka = 50.8;
+        double angka2 = 60.77;
+        double angka3 = angka*angka2;
+        double angka4 = angka/angka2;
+        System.out.println("jadi hasil dari 50.8 × 60.77 = "+angka3);
+        System.out.println("jadi hasil dari 50.8 / 60.77 = "+angka4);
+    }
+    
+}
