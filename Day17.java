f44d634635903374f418ff99b59ba49aba5a0bb6
Plidesta <lidescraft@gmail.com>
2025-09-24 20:09:17 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,9 @@
+public class main {
+  public static void main(String[]args) {
+       int nilai = 12 , nilai2 = 23;
+        double nilai3 = 20.99 , nilai4 = 50.88;
+        System.out.println("modulus dari nilai % nilai2 = "+(nilai%nilai2));
+        System.out.println("modulus dari nilai3 % nilai4 = "+(nilai3%nilai4));
+    }
+    
+}
