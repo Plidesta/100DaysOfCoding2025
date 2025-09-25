9fbeb93042941593ea49a938f29f0c3f551e1b29
Plidesta <lidescraft@gmail.com>
2025-09-25 21:16:28 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,17 @@
+public class main {
+  public static void main(String[]args) {
+      byte myByte = 80;
+        short myShort = myByte;
+        int myint = myShort;
+        long myLong = myint;
+        float myFloat = myLong;
+        double myDouble = myFloat;
+      
+        System.out.println(myShort);
+        System.out.println(myint);
+        System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+    }
+    
+}
