d183bcbcc2f52d906be5252d4bd0e1c5e050195c
Plidesta <lidescraft@gmail.com>
2025-09-26 20:44:42 +0800

    Create Day19.java

---
+++ Day19.java
@@ -0,0 +1,18 @@
+public class main {
+  public static void main(String[]args) {
+     byte bt = 50;
+      short sh = (short)bt;
+        int in = (int)sh;
+          long lg = (long)in;
+        double dl = (double)lg;
+            float fl = (float)dl;
+        System.out.println("");
+        System.out.println(bt);
+        System.out.println(sh);
+        System.out.println(in);
+        System.out.println(lg);
+        System.out.println(dl);
+        System.out.println(fl);
+    }
+    
+}
