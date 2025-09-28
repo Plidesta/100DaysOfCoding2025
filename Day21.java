fe0a1f440f14c08b036bc3b7089289ebba988697
Plidesta <lidescraft@gmail.com>
2025-09-28 11:37:23 +0800

    Update Day21.java

--- Day21.java
+++ Day21.java
@@ -2,9 +2,9 @@ public class main {
   public static void main(String[]args) {
        int Va = 80 , Vb = 8;
         System.out.println("sebelum pertukaran : \nVa = "+Va+" & Vb = "+Vb);   
-        Va = Vb + Va;
-        Vb = Va - Vb;
-        Va = Va - Vb;
+        Va = Vb + Va;//hasilnya = 88
+        Vb = Va - Vb;//88 - 8 = 80
+        Va = Va - Vb;//88 - 80 = 8
         System.out.println("setelah pertukaran : \nVa = "+Va+" & Vb = "+Vb);
     }
     
