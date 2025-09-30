9806160d984f8e51aae79f1d9359e19354709009
Plidesta <lidescraft@gmail.com>
2025-09-30 18:30:44 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class day22 {
+    public static void main(String[]args) {
+     Scanner v = new Scanner(System.in);
+        System.out.print("masukan nilai panjang : ");
+     int panjang= v.nextInt();
+     System.out.print("masukan nilai lebar : ");
+     int lebar = v.nextInt();
+   System.out.println("luas persegi panjang X = "+(panjang*lebar));
+    }
+    
+}
