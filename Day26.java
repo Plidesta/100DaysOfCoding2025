2d2114e4e6e9286b806cc7c0a24d8a8fc66c5310
Plidesta <lidescraft@gmail.com>
2025-10-03 09:03:00 +0800

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,33 @@
+public class main {
+    public static void main(String[] args) {
+       int a= 20;
+        
+        a += 5 ;
+        System.out.println("nilai setelah a +=5 = "+a);
+        System.out.println("");
+        int b = 20;
+        b -= 10;
+        System.out.println("nilai setelah b -=10 = "+b);
+        System.out.println("");
+        int c = 20;
+        c *= 2;
+        System.out.println("nilai setelah c *=2 = "+c);
+        System.out.println("");
+       int d = 20;
+        d /= 2;
+        System.out.println("nilai setelah d /=2 = "+d);
+        System.out.println("");
+        int e = 20;
+        e %= 20;
+        System.out.println("nilai setelah e %=20 = "+e);
+        System.out.println("");
+        int f = 20;
+        f >>= 2;
+        System.out.println("nilai setelah f >>=2 = "+f);
+        System.out.println("");
+        int g = 20;
+        g <<= 2;
+        System.out.println("nilai setelah g <<=2 = "+g);
+    }
+    
+  }
