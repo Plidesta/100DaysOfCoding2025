d35e41ef34ce537d9d1195ab703dde4e821ada9a
Plidesta <lidescraft@gmail.com>
2025-10-10 21:43:47 +0800

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,12 @@
+public class main {
+  public static void main(String[]args) {
+        boolean nilai = true;
+        boolean nilai2 = false;
+        System.out.println("nilai dari awal kondisi satu = "+nilai);
+        System.out.println("nilai dari awal kondisi dua = "+nilai2);
+        System.out.println("lalu dengan oporator logika not dibalik menjadi: ");
+        System.out.println("nilai akhir kondisi satu = "+!nilai);
+        System.out.println("nilai akhir kondisi dua = "+!nilai2);
+    }
+    
+}
