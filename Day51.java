de73d2664e8bdafc355c00a3cd7875e79db9c73a
Plidesta <lidescraft@gmail.com>
2025-10-28 19:19:29 +0800

    Create Day51.java

---
+++ Day51.java
@@ -0,0 +1,10 @@
+public class day51 {
+    
+    public static void main(String[] args) {
+        for (int lids = 10;lids <=100;lids+=10) {
+            System.out.print(lids+" ");
+        
+        }
+    }
+    
+}
