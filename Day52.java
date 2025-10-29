7a9a4273bf8c92122dcff0e9ee3b02f232f4b5ea
Plidesta <lidescraft@gmail.com>
2025-10-29 18:18:36 +0800

    Create Day52.java

---
+++ Day52.java
@@ -0,0 +1,12 @@
+public class dau255 {
+    
+    public static void main(String[]args) {
+        int nilaiA = 1; 
+
+        while (nilaiA <= 50) {
+            System.out.println(nilaiA); 
+            nilaiA++; 
+        }
+        System.out.println("Perulangan selesai.");
+    }
+}
