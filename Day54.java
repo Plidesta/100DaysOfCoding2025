846dcfc429e53ea14cdab7723682e15b352a4d40
Plidesta <lidescraft@gmail.com>
2025-10-31 19:06:29 +0800

    Create Day54.java

---
+++ Day54.java
@@ -0,0 +1,14 @@
+public class day54 {
+    
+    public static void main(String[] args) {
+        for (int x = 1;x<=100;x++) {
+            if (x%2==1){
+                continue;
+            }
+            System.out.println(x);
+        
+        }
+        
+    }
+    
+}
