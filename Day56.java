e877ae5532abd75670cc409c254407647e7c1782
Plidesta <lidescraft@gmail.com>
2025-11-02 21:27:06 +0800

    Create Day56.java

---
+++ Day56.java
@@ -0,0 +1,14 @@
+public class main {
+    
+    public static void main(String[] args) {
+        
+        int awal = 1;
+        
+        do {
+            System.out.println(awal);
+            awal++;
+        }while (awal <=50);
+        System.out.println("program selesai");
+    }
+    
+}
