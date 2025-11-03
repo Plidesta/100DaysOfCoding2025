22bfbef95e565cbd328c630ffd4818d527d2e79e
Plidesta <lidescraft@gmail.com>
2025-11-03 22:09:30 +0800

    Create Day57.java

---
+++ Day57.java
@@ -0,0 +1,16 @@
+public class dyyyy {
+    
+    public static void main(String[] args) {
+        System.out.println("nasted loop");
+        
+for (int a = 1; a <= 5; a++) {
+    for (int b = 1; b <= 5; b++) {
+        System.out.print("(" + a + "," + b + ") ");
+    }
+    
+    System.out.println(); 
+}
+        
+         }
+    
+}
