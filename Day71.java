0706ec1f4c23c86ec01b760576665e27a052ae2c
Plidesta <lidescraft@gmail.com>
2025-11-17 23:18:28 +0800

    Create Day71.java

---
+++ Day71.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Scanner v = new Scanner(System.in);
+        System.out.print("Masukkan tinggi segitiga : ");
+        int tinggi = v.nextInt();
+
+        
+        if (tinggi <=18&&tinggi>=5){
+        System.out.println("");
+        System.out.println("===========================");
+        for (int a = 1; a <= tinggi; a++) {
+            for (int b = 1; b <= a; b++) {
+                System.out.print("☆ ");
+            }
+            System.out.println(); 
+        }
+        
+        }else {
+            System.out.println("angka melebihi batas");
+        }
+        
+    }
+}
