ae18d4255902d210a3e14219137a9bcfcb88ea75
Plidesta <lidescraft@gmail.com>
2025-11-21 17:19:26 +0800

    Create Day75 .java

---
+++ Day75 .java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+        String a = "latihan string dasar";
+        
+        int panjang = a.length();
+        System.out.println("Panjang String: "+panjang); 
+        
+        String besar = a.toUpperCase();
+        System.out.println("Huruf Besar: "+besar);
+        
+        String kecil = a.toLowerCase();
+        System.out.println("Huruf Kecil: "+kecil);
+        
+    }
+}
