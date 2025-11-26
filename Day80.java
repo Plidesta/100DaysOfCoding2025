3665cb939e19f1a2442692aa9d674cb5a2fb96b2
Plidesta <lidescraft@gmail.com>
2025-11-26 19:58:17 +0800

    Create Day80.java

---
+++ Day80.java
@@ -0,0 +1,9 @@
+public class day78 {
+    public static void main(String[] args) {
+        String[]namaHewan = {"babi", "ayam", "kucing", "ular", "sapi", "cacing", "kodok",};
+        for (String oke : namaHewan){
+            System.out.print(oke+", ");
+        }
+        
+    }
+}    
