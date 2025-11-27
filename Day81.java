8ce9c40481e3b91e0998206cfa52d6fa1a613a87
Plidesta <lidescraft@gmail.com>
2025-11-27 22:52:04 +0800

    Create Day81.java

---
+++ Day81.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        int[]bilGanjil = {3,5,7,11,89,99,1};
+        System.out.println("indeks 0 = "+bilGanjil[0]);
+        System.out.println("indeks 1 = "+bilGanjil[1]);
+        System.out.println("indeks 2 = "+bilGanjil[2]);
+        System.out.println("indeks 3 = "+bilGanjil[3]);
+        System.out.println("indeks 4 = "+bilGanjil[4]);
+        System.out.println("indeks 5 = "+bilGanjil[5]);
+        System.out.println("indeks 6 = "+bilGanjil[6]);
+    }
+}    
