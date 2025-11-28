c23281eff5b93b08c52190a4e230c0e47caac5ce
Plidesta <lidescraft@gmail.com>
2025-11-28 18:51:02 +0800

    Create Day82.java

---
+++ Day82.java
@@ -0,0 +1,19 @@
+public class main {
+    public static void main(String[] args) {
+       int[]angka = new int[10];
+        angka[0]=10;
+        angka[1]=20;
+        angka[2]=30;
+        angka[3]=40;
+        angka[4]=50;
+        angka[5]=60;
+        angka[6]=70;
+        angka[7]=80;
+        angka[8]=90;
+        angka[9]=100;
+        for (int oke:angka){
+            System.out.print(oke+", ");
+        }
+        
+    }
+}    
