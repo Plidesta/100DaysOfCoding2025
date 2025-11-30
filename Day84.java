10ffde7c45708af6669b250fb308c80fbdfbf2da
Plidesta <lidescraft@gmail.com>
2025-11-30 21:27:58 +0800

    Create Day84.java

---
+++ Day84.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class main {
+    
+    public static void main(String[] args) {
+        Scanner v = new Scanner(System.in);
+        String kata = v.nextLine();
+        int n = v.nextInt();
+        
+        for (int i=0;i<=n;i++){
+            System.out.println(kata);
+        }
+    }
+    
+}
