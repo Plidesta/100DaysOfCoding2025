7b922af9f4ad459726d051310b775c308b0ea444
Plidesta <lidescraft@gmail.com>
2025-12-05 19:56:00 +0800

    Create Day89.java

---
+++ Day89.java
@@ -0,0 +1,18 @@
+public class day88 {
+    public void oke(String nama){
+        System.out.print("halo nama saya "+nama);
+    }public void mantap(String nim){
+      System.out.print(" \ndengan nim "+nim);
+    }public void sip(String prodi){
+      System.out.println(" \ndari prodi "+prodi);
+    }
+    public static void main(String[]args){
+        day88 method = new day88();
+        System.out.println("------------‐-------");
+        method.oke("PLIDESTA");
+        method.mantap("D0225312");
+        method.sip("INFORMATIKA");
+        System.out.println("----‐---------------");
+    }
+    
+}
