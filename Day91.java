13b181cd412efb5e0cee4a08c858d3bb82131221
Plidesta <lidescraft@gmail.com>
2025-12-07 20:28:33 +0800

    Create Day91.java

---
+++ Day91.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void data(String nama, int angkatan) {
+        System.out.println("Halo nama saya " + nama + " dari angkatan " + angkatan);
+    }
+
+    public static void main(String[] args) {
+        data("ceking", 2025);  
+        data("agus", 2022);   
+    }
+}
