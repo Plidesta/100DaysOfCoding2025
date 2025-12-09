1c2610db751127180080ce98aac594d5c8211702
Plidesta <lidescraft@gmail.com>
2025-12-10 01:23:18 +0800

    Create Day94.java

---
+++ Day94.java
@@ -0,0 +1,21 @@
+public class main {
+    public void garis(){
+        System.out.print("-------------------------");
+    }
+    public void nama(){
+        System.out.print("\nPlidesta");
+    }
+    public void prodi(){
+        System.out.println("\ndari prodi informatika");
+    }
+    public static void main(String[]args){
+        day94 oke = new day94();
+        
+        oke.garis();
+        oke.nama();
+        oke.prodi();
+        oke.garis();
+        
+
+    }
+}
