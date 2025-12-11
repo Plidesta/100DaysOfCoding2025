f7076f5f1da5d0790b1ca2e77023f07d287c3897
Plidesta <lidescraft@gmail.com>
2025-12-11 12:36:35 +0800

    Create Day95.java

---
+++ Day95.java
@@ -0,0 +1,17 @@
+public class main {
+    public void pesan1(String nama, String kata){
+        System.out.println("");
+        System.out.println("#####################");
+        System.out.println("----------‐----------");
+        System.out.println("");
+        System.out.println("halo "+nama+" ISC "+kata+" sisa 5 hari lagi");
+        System.out.println("");
+        System.out.println("---‐-----------------");
+        System.out.println("#####################");
+        System.out.println("");
+    }public static void main(String[]args){
+        day94 oke = new day94();
+        
+        oke.pesan1("plidesta","100 Days of coding");
+    }
+}
